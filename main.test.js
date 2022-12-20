be564fd0fd2af155e9901b6adc694506f46098af
Daniel A.C. Martin <github@daniel-martin.co.uk>
2022-12-20 17:25:20 +0000

    Demonstrate broken tests
    The code is run but the result is discarded. Meanwhile, an assertion is
    made but it can never fail as it is run on a non-empty string.
    
    FWIW, Red-Green testing or TDD would have picked this up.

--- main.test.js
+++ main.test.js
@@ -26,14 +26,14 @@ describe('Pokemon evolution chain', () => {
   }).timeout(5000);
 
   it("error status code is shown when an empty argument is provided", async () => {
-    await pokemon.run('');
+    // await pokemon.run('');
     const expected = 'Status code: 404 with error message Not Found';
-    console.assert({ expected });
+    console.assert({ expected }); // This can never fail!
   }).timeout(5000);
 
   it("error status code is shown when we provide fake pokemon", async () => {
-    await pokemon.run('superman');
+    // await pokemon.run('superman');
     const expected = 'Status code: 404 with error message Not Found';
-    console.assert({ expected });
+    console.assert({ expected }); // This can never fail!
   }).timeout(5000);
 });
